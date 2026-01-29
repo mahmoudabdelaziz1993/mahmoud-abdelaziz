0ff83d7d0d9371047b42778b7faccc2c57b8331a
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2026-01-30 01:17:54 +0200

    Add  new Certifications section

--- components/blocks/certifications.tsx
+++ components/blocks/certifications.tsx
@@ -1,45 +1,88 @@
 import { cn } from "@/lib/utils";
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import Image from "next/image";
+import Link from "next/link";
 
+/*************  ✨ Windsurf Command ⭐  *************/
+/**
+ * A component to display a list of certifications.
+ *
+ * @param {string} [className] - The CSS class name for the component.
+ * @param {string} [headline] - The headline to display above the list of certifications.
+ * @param {object[]} certifications - An array of certification objects.
+ * @param {string} certifications.title - The title of the certification.
+ * @param {string} certifications.link - The link to the certification.
+ * @param {string} certifications.image - The URL of the image of the certification.
+ * @param {string} certifications.date - The date of the certification.
+ * @param {string} certifications.institution - The institution that issued the certification.
+ * @returns {React.ReactElement} - The rendered component.
+ */
+/*******  6b71ccf8-3b9e-4c25-830b-466b3b8d9ca5  *******/
 export default function Certifications({
     // experience,
     className,
-    headline
+    headline,
+    certifications,
 }: {
     className?: string,
     headline?: string,
+    certifications: {
+        title: string;
+        link: string;
+        image: string;
+        date: string;
+        institution: string;
+    }[]
 
 }) {
+
+
+
+
     return (
-        <section className={cn("py-32", className)}>
-            <div className="container space-y-10 lg:space-y-20">
-                <div className="flex w-full items-end justify-between">
-                    <h1 className="text-5xl font-semibold tracking-tighter ">
+        <section className={cn("py-32 w-full", className)} >
+            <div className="container  space-y-10 lg:space-y-20">
+                <div className="flex w-full items-end justify-between ">
+                    <h1 className="text-5xl font-semibold tracking-tighter capitalize ">
                         {headline}
                     </h1>
-                    {/* <Button variant="ghost" size="lg" className="font-semibold">
-                    Download CV <Download className="size-4" />
-                </Button> */}
                 </div>
-
-                {/* <ul>
-                    {experience.map((exp, index) => (
+                <ul>
+                    {certifications.map((certification, index) => (
                         <li
                             key={index}
                             className="flex flex-col justify-between border-b py-10 md:flex-row"
                         >
                             <div className="max-w-lg text-xl font-semibold tracking-tighter lg:w-1/3">
-                                {exp.period}
+                                {certification.date}
                             </div>
                             <div className="lg:w-1/3">
-                                <h2 className="mb-4 text-2xl font-semibold tracking-tighter">
-                                    {exp.title}
-                                </h2>
-                                <p className="text-foreground/50">{exp.description}</p>
+                                <HoverCard>
+                                    <HoverCardTrigger asChild>
+                                        <Link href={certification.link} target="_blank" className="mb-4 text-2xl font-semibold tracking-tighter">
+                                            {certification.title}
+                                        </Link>
+                                    </HoverCardTrigger>
+                                    <HoverCardContent>
+                                        <Image
+                                            src={certification.image}
+                                            alt={certification.title}
+                                            width={500}
+                                            height={500}
+                                        />
+                                    </HoverCardContent>
+                                </HoverCard>
+
                             </div>
-                            <div className="text-right lg:w-1/4">{exp.company}</div>
+                            <div className="text-right lg:w-1/4">{certification.institution}</div>
                         </li>
                     ))}
-                </ul> */}
+                </ul>
+
             </div>
         </section>
     )
---
+++ data/certificationsContent.ts
@@ -0,0 +1,73 @@
+import { title } from "process";
+
+export const CertificationsSectionContent = {
+    headline: {
+        en: "Certifications",
+        ar: "الشهادات"
+    },
+    certifications: [
+        {
+            title: {
+                en: "React Development Cross-Skilling Nanodegree",
+                ar: "برنامج تطوير مهارات متعددة في React"
+            },
+            institution: {
+                en: "Udacity MENA",
+                ar: "Udacity الشرق الأوسط وشمال أفريقيا"
+            },
+            date: {
+                en: "27 Jul 2022",
+                ar: "27 يوليو 2022"
+            },
+            url: "https://drive.google.com/file/d/1LZpx9HPqRISsXVb01LrEChI06uSSyW62/view",
+            image: "/images/certificates/udacity.png"
+        }, {
+            title: {
+                en: "Legacy JavaScript Algorithms and Data Structures V7",
+                ar: " خوارزميات وهياكل بيانات جافا سكريبت القديمة الإصدار 7"
+            },
+            institution: {
+                en: "freecodecamp.org",
+                ar: "freecodecamp.org"
+            },
+            date: {
+                en: "24 February  2023",
+                ar: "24 فبراير 2023"
+            },
+            url: "https://www.freecodecamp.org/certification/fcc5cbca4a4-ab19-4f2c-abf6-9179c4bc4131/javascript-algorithms-and-data-structures",
+            image: "/images/certificates/freecodecampJsAlgoDs.png"
+        },
+        {
+            title: {
+                en: "Legacy Responsive Web Design V8",
+                ar: " تصميم مواقع الويب متجاوب  الإصدار 8"
+            },
+            institution: {
+                en: "freecodecamp.org",
+                ar: "freecodecamp.org"
+            },
+            date: {
+                en: "7 Mar 2023",
+                ar: "7 مارس 2023"
+            },
+            url: "https://www.freecodecamp.org/certification/fcc5cbca4a4-ab19-4f2c-abf6-9179c4bc4131/responsive-web-design",
+            image: "/images/certificates/freecodecampRWD.png"
+        },
+        {
+            title: {
+                en: "Front End Development Libraries V8",
+                ar: "مكتبات تطوير الواجهة الأمامية الإصدار 8"
+            },
+            institution: {
+                en: "freecodecamp.org",
+                ar: "freecodecamp.org"
+            },
+            date: {
+                en: "17 Mar 2023",
+                ar: "17 مارس 2023"
+            },
+            url: "https://www.freecodecamp.org/certification/fcc5cbca4a4-ab19-4f2c-abf6-9179c4bc4131/front-end-development-libraries",
+            image: "/images/certificates/freecodecampFEDL.png"
+        }
+    ]
+}
