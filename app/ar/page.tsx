7cd9ba203dad28995eb6f768ed8589619c66fc28
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2026-02-03 02:41:40 +0200

    Update Hero section UI and content
    - Added language support to Hero section content
    - Updated Hero section styling and layout
    - Added download button for resume
    - Fixed alignment and responsiveness of Hero section components

--- app/ar/page.tsx
+++ app/ar/page.tsx
@@ -3,6 +3,7 @@ import { Experience1 } from "@/components/blocks/experience";
 import { Footer } from "@/components/blocks/footer";
 import { Header } from "@/components/blocks/Header";
 import { HeroSection } from "@/components/blocks/hero";
+import SectionLayout from "@/components/layout/section-layout";
 import { CertificationsSectionContent } from "@/data/certificationsContent";
 import { ExperienceSectionContent } from "@/data/ExperienceSectionContent";
 
@@ -15,24 +16,28 @@ export default function ArPage() {
             {/* Hero */}
             <HeroSection lang={lang} />
             {/* experience */}
-            <Experience1 headline={ExperienceSectionContent.headline[lang]} experience={ExperienceSectionContent.experiences.map(item => ({
-                period: item.period[lang],
-                title: item.title[lang],
-                company: item.company[lang],
-                description: item.description[lang],
-            }))} />
-            {/* certifications  */}
-            <Certifications headline={CertificationsSectionContent.headline[lang]}
-                certifications={CertificationsSectionContent.certifications.map(item => ({
+            <SectionLayout sectionId={ExperienceSectionContent.key} sectionTitle={ExperienceSectionContent.headline[lang]}>
+                <Experience1 experience={ExperienceSectionContent.experiences.map(item => ({
+                    period: item.period[lang],
                     title: item.title[lang],
-                    link: item.url,
-                    image: item.image,
-                    date: item.date[lang],
-                    institution: item.institution[lang]
+                    company: item.company[lang],
+                    description: item.description[lang],
+                }))} />
+            </SectionLayout>
+            {/* certifications  */}
+            <SectionLayout sectionId={CertificationsSectionContent.key} sectionTitle={CertificationsSectionContent.headline[lang]}>
+                <Certifications
+                    certifications={CertificationsSectionContent.certifications.map(item => ({
+                        title: item.title[lang],
+                        link: item.url,
+                        image: item.image,
+                        date: item.date[lang],
+                        institution: item.institution[lang]
 
 
-                }))}
-            />
+                    }))}
+                />
+            </SectionLayout>
             {/* Footer */}
             <Footer lang={lang} />
         </>
---
+++ data/skils.ts
@@ -0,0 +1,14 @@
+export const SkilsContent = {
+    key: "Technical-Skills",
+    headline: {
+        en: "Technical Skills",
+        ar: "المهارات التقنية"
+    },
+
+    list: [
+        "JavaScript (ES6+)", "TypeScript", "HTML5", "CSS3", "Sass/LESS",
+        "React.js", "Next.js", "Node.js", "Next.js", "GraphQL",
+        "Parcel", "Webpack", "Vite", "Yarn", "NPM",
+        "MongoDB", "PostgreSQL", "Firebase", "Supabase", "Docker",
+    ]
+}
